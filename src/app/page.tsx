fd2249f2429c0190df4d2a56b7f9aba66d02c52a
Kapee87 <kapserker@gmail.com>
2024-05-02 13:14:20 -0300

    Starting learning project for Next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
-export default function HomePage() {
-  return <section>👋</section>;
+import api from "@/api";
+import RestaurantCard from "./components/RestaurantCard";
+
+export default async function HomePage() {
+  const restaurants = await api.list();
+  console.log(restaurants);
+
+  return (
+    <section className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-3">
+      {
+        restaurants.map((restaurant) => {
+          return (
+            <RestaurantCard restaurant={restaurant} />
+          )
+        })
+      }
+
+    </section>
+  );
 }
